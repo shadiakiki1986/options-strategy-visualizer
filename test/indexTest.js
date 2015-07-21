8e4a98e2f3c92453be635b9708d102058e9f101a
Shadi Akiki <shadiakiki1986@gmail.com>
2015-07-21 09:03:48 +0000

    added index entry point and testing [ci skip]

---
+++ test/indexTest.js
@@ -0,0 +1,17 @@
+'use strict';
+var OptStratVis = require('app/index');
+var should = require('should');
+
+describe.only('index tests', function() {
+  
+  it('covered call short', function() {
+    var x=OptStratVis([
+        { q:  1, t:'C', X:80, r:0, v:0.25, T:0}, // [{q: 1, t:'P', X:80, r:0, v:0.25, T:0, V:2.12}]
+        { q: -1, t:'S', X:80, r:0, v:0.25, T:0}
+      ],
+      [60,80,100,120],
+      0);
+    (x==80).should.eql(true);
+  });
+
+});
